a29792507f3782ce4c246ccf1702cfd3f2aeb5ff
mohamadZareai <mohamadzareai@gmail.com>
2020-12-11 02:45:22 +0330

    creat array obj react

---
+++ src/Post.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Post =(props)=>{
+    return <div>
+    <h3>{props.name}</h3>
+    <p>{props.lastName}</p>
+  </div>;
+}
+
+export default Post;
--- src/Section.js
+++ src/Section.js
@@ -1,12 +1,24 @@
 import React from "react";
-
+import Post from "./Post";
+const posts=[{
+name:"mohamad",
+lastName:"zarei",
+},
+{ name:"Radshodam",
+    lastName:"Radsho",
+    },
+{ name:"enzo",
+    lastName:"Radsho",
+    }]
 const Section = () => {
   return (
     <div className={"sect"}>
       <h1>section1</h1>
-      <h1>section2</h1>
-      <h1>section3</h1>
-      <h1>section4</h1>
+{
+    posts.map(post=>{
+        return <Post name={post.name} lastName={post.lastName}/>
+    })
+}
     </div>
   );
 };
